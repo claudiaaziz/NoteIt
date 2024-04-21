100edf0fec550e3dfcc40c75675e2ac8d75c912a
claudiaaziz <claudiahannahh@gmail.com>
2024-04-21 19:46:26 -0400

    Finish styling notes

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,19 +1,30 @@
 import { Card } from 'react-bootstrap';
 import { Note as NoteModel } from '../models/notes';
-import styles from '../styles/Note.module.css'
+import styles from '../styles/Note.module.css';
+import { formatDate } from '../utils/formatDate';
 
 interface NoteProps {
   note: NoteModel;
+  className?: string;
 }
 
-const Note = ({ note }: NoteProps) => {
+const Note = ({ note, className }: NoteProps) => {
   const { title, body, createdAt, updatedAt } = note;
+
+  let createdUpdatedText: string; 
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated: " + formatDate(updatedAt)
+  } else {
+    createdUpdatedText = "Created: " + formatDate(createdAt)
+  }
+
   return (
-    <Card className={styles.noteCard}>
-      <Card.Body>
+    <Card className={`${styles.noteCard} ${className}`}>
+      <Card.Body className={styles.cardBody}>
         <Card.Title>{title}</Card.Title>
-        <Card.Text className={styles.cardBody}>{body}</Card.Text>
+        <Card.Text className={styles.cardText}>{body}</Card.Text>
       </Card.Body>
+      <Card.Footer className='text-muted'>{createdUpdatedText}</Card.Footer>
     </Card>
   );
 };
