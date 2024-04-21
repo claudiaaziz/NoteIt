594231cf1b29e8867a5f5536044756e1f8bb21f7
claudiaaziz <claudiahannahh@gmail.com>
2024-04-20 20:59:30 -0400

    MongoDB Atlas & Mongoose setup & Envalid

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
-import express from 'express';
-const app = express();
-const port = 5005;
+import app from './app';
+import env from './util/validateEnv';
+import mongoose from 'mongoose';
 
-app.get('/', (req, res) => {
-  res.send('🩷');
-});
+const port = env.PORT;
+const mongoConnectionString = env.MONGO_CONNECTION_STRING;
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+mongoose
+  .connect(mongoConnectionString)
+  .then(() => {
+    console.log('Mongoose connected');
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  })
+  .catch(console.error);
