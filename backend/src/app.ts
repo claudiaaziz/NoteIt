175f22035553b39087e4572706cae6ca1b8c518f
claudiaaziz <claudiahannahh@gmail.com>
2024-05-06 18:21:28 -0400

    User auth w express-session

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,32 @@
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import notesRoutes from './routes/notes';
+import userRoutes from './routes/users';
 import morgan from 'morgan';
 import createHttpError, { isHttpError } from 'http-errors';
+import session from 'express-session';
+import env from './util/validateEnv';
+import MongoStore from 'connect-mongo';
 
 const app = express();
 
-app.use(express.json());
-
 app.use(morgan('dev'));
 
+app.use(express.json());
+
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+    rolling: true,
+    store: MongoStore.create({ mongoUrl: env.MONGO_CONNECTION_STRING }),
+  })
+);
+
 app.use('/api/notes', notesRoutes);
+app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
   next(createHttpError(404, 'Endpoint not found'));
@@ -21,8 +37,8 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   let errorMsg = 'An unknown error occured.';
   let statusCode = 500;
   if (isHttpError(error)) {
-    statusCode = error.status
-    errorMsg = error.message
+    statusCode = error.status;
+    errorMsg = error.message;
   }
   res.status(statusCode).json({ error: errorMsg });
 });
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import * as UserController from '../controllers/users'
+const router = express.Router()
+
+router.get('/', UserController.getAuthenticatedUser)
+router.post('/signup', UserController.signUp)
+router.post('/signin', UserController.signIn)
+router.post('/signout', UserController.signOut)
+
+export default router;
